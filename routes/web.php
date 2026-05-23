7c612aa96953689d9df4c50ade322a935659d445
Design <design@fahad-Imacs.local>
2026-05-23 15:40:46 +0300

    add about page

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,6 @@ Route::get('/', function () {
 });
 
 Route::get('/cafeteria', function () {
-
     $products = Product::where('active', true)->latest()->get();
 
     return view('cafeteria', [
@@ -20,22 +19,6 @@ Route::get('/artists', function () {
     return view('artists');
 });
 
-Route::get('/artists/abdulaziz', function () {
-    return view('artists.abdulaziz');
-});
-
-Route::get('/artists/buthaina', function () {
-    return view('artists.buthaina');
-});
-
-Route::get('/artists/shihab', function () {
-    return view('artists.shihab');
-});
-
-Route::get('/artists/mais', function () {
-    return view('artists.mais');
-});
-
-Route::get('/artists/abdullah', function () {
-    return view('artists.abdullah');
+Route::get('/about', function () {
+    return view('about');
 });
